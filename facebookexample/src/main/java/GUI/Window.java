3d0660252d016cd5faf54761501f4615d59fe9bc
Varela <nvrls@iscte-iul.pt>
2018-10-25 15:07:13 +0100

    GUI Update 25/10 15h

--- facebookexample/src/main/java/GUI/Window.java
+++ facebookexample/src/main/java/GUI/Window.java
@@ -1,8 +1,10 @@
 package GUI;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
@@ -19,6 +21,9 @@ import javax.swing.WindowConstants;
 public class Window {
 	
 	private JFrame frame;
+	private boolean facebook = false;
+	private boolean twitter = false;
+	private boolean email = false;
 	
 	public Window() {
 		frame = new JFrame("Test");
@@ -49,40 +54,22 @@ public class Window {
 	}
 	
 	private void addFrameContent() {
-		//Menu
-		JMenuBar menuBar = new JMenuBar();
-		frame.add(menuBar, BorderLayout.NORTH);
-		JMenu menu = new JMenu("Menu");
-		menuBar.add(menu);
-		
-		//Painel Redes Sociais, etc..
-		JPanel p = new JPanel();
-		frame.add(p, BorderLayout.WEST);
-		p.setLayout(new GridLayout(3,1));
-		
-		JButton Facebook = new JButton("Facebook");
-		p.add(Facebook);
-		
-		JButton Twitter = new JButton("Twitter");
-		p.add(Twitter);
-		
-		JButton Email = new JButton ("E-mail");
-		p.add(Email);
-		
+		/*
 		//Painel Login, Sign-up;
-		//Login: e-mail, password;
-		//Sign-up:e-mail, password, confirm password; 
 		JPanel p1 = new JPanel();
 		frame.add(p1, BorderLayout.CENTER);
 		
+		//Login: e-mail, password;
 		JButton Login = new JButton("Login");
 		Login.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 			}
 		});
-		p1.add(Login, BorderLayout.CENTER);
-		
+		//p1.add(Login, BorderLayout.CENTER);
+		*/
+		//Sign-up:e-mail, password, confirm password;
+		/*
 		JButton Sign = new JButton("Sign-Up");
 		Sign.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -90,7 +77,72 @@ public class Window {
 			}
 		});
 		p1.add(Sign, BorderLayout.CENTER);
+		*/ 
 		
+		//Menu
+		JMenuBar menuBar = new JMenuBar();
+		frame.add(menuBar, BorderLayout.NORTH);
+		JMenu menu = new JMenu("Menu");
+		menuBar.add(menu);
+		
+		//Painel das Redes Sociais
+		JPanel p = new JPanel();
+		frame.add(p, BorderLayout.WEST);
+		p.setLayout(new GridLayout(3,1));
+		
+		JButton Facebook = new JButton("Facebook");
+		Facebook.setBackground(new Color(59, 89, 182));
+	    Facebook.setForeground(Color.WHITE);
+	    Facebook.setFocusPainted(false);
+	    Facebook.setFont(new Font("Tahoma", Font.BOLD, 20));
+	    Facebook.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				facebookActivate();
+			}
+		});
+		p.add(Facebook);
+		
+		JButton Twitter = new JButton("Twitter");
+		Twitter.setBackground(new Color(29, 202, 255));
+	    Twitter.setForeground(Color.WHITE);
+	    Twitter.setFocusPainted(false);
+	    Twitter.setFont(new Font("Tahoma", Font.BOLD, 20));
+		p.add(Twitter);
+		
+		JButton Email = new JButton ("E-mail");
+		Email.setBackground(new Color(178, 49, 33));
+	    Email.setForeground(Color.WHITE);
+	    Email.setFocusPainted(false);
+	    Email.setFont(new Font("Tahoma", Font.BOLD, 20));
+		p.add(Email);	
+	}
+	
+	public void facebookActivate() {
+		if(facebook == false) {
+			//Painel Login, Sign-up;
+			JPanel p1 = new JPanel();
+			
+			//Login: e-mail, password;
+			JButton Login = new JButton("Login");
+			Login.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					
+				}
+			});
+			p1.add(Login);
+			frame.add(p1, BorderLayout.CENTER);
+			
+			facebook = true;
+			twitter = false;
+			email = false;
+		}
+		frameRef();
+	}
+	
+	public void frameRef() {
+		frame.invalidate();
+		frame.validate();
+		frame.repaint();
 	}
 	
 	public static void main(String[] args) {
